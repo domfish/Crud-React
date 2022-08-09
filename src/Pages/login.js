8d04b028a21c2c81b14b91e56079078c522f0f5b
Chiheb mejri <chihebmjrr@gmail.com>
2022-08-09 14:41:12 +0100

    second commit : state added

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function Login() {
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+const handleChange=(e)=>{
+    setPassword(e.target.value)
+}
+const handleEmail=(e)=>{
+    setEmail(e.target.value)
+}
     return (
         <div className='row'>
             <h1 className='text-center text-primary'>Welcome</h1>
             <form className='offset-md-3 col-md-6'>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" value={email} onChange={handleEmail} />
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" value={password} onChange={handleChange} />
                 </div>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Remembre Me</label>
+                    <input type="checkbox" class="form-check-input" id="remember" />
+                    <label class="form-check-label" for="remember">Remembre Me</label>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
--- src/Pages/register.js
+++ src/Pages/register.js
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Register() {
+
+    const [data, setData] = useState({
+            nom: '',
+            prenom: '',
+            adressEmail: '',
+            password: '',
+        });
+
+        const handleChangeName =(e)=>{
+            setData({
+                ...data,
+                nom : e.target.value
+            }) 
+        }
+        const handleChangePreNom =(e)=>{
+            setData({
+                ...data,
+                prenom : e.target.value
+            }) 
+        }
+        const handleChangeEmail =(e)=>{
+            setData({
+                ...data,
+                adressEmail : e.target.value
+            }) 
+        }
+        const handleChangePassword =(e)=>{
+            setData({
+                ...data,
+                password : e.target.value
+            }) 
+        }
     return (
         <div className="row">
-            
+            <h1 className="text-center text-primary">Join Us</h1>
             <form className="offset-md-3 col-md-6">
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Nom</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <label for="nom" class="form-label">Nom</label>
+                    <input type="text" class="form-control" id="nom" value={data.nom} onChange={handleChangeName}/>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Prenom</label>
-                    <input type="text" class="form-control" id="exampleInputPassword1"/>
+                    <label for="prenom" class="form-label">Prenom</label>
+                    <input type="text" class="form-control" id="prenom" value={data.prenom} onChange={handleChangePreNom}/>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Adresse mail</label>
-                    <input type="email" class="form-control" id="exampleInputPassword1"/>
+                    <label for="email" class="form-label">Adresse mail</label>
+                    <input type="email" class="form-control" id="email" value={data.adressEmail} onChange={handleChangeEmail}/>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1"/>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" value={data.password} onChange={handleChangePassword}/>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1"/>
+                    <label for="confirmPaswword" class="form-label">Confirm Password</label>
+                    <input type="password" class="form-control" id="confirmPaswword" />
                 </div>
                 <div className="d-grid gap-2">
                     <button type="submit" className="btn btn-primary">Submit</button>
